f48b555f35bdb2e3af9e5910aa83e73eac79ff2f
balakrishna-k <32950521+balakrishna-k@users.noreply.github.com>
2018-11-10 16:29:45 -0800

    Utility functions work (#5)

--- src/ChordNetwork.java
+++ src/ChordNetwork.java
@@ -1,7 +1,43 @@
 public class ChordNetwork {
 
     public static void main(String[] args) {
-        Node node1 = new Node(256);
-        Node node2 = new Node(1);
+        // Testing findSuccessor, findPredecessor and closestPrecedingFinger
+        Node node0 = new Node(0);
+        FingerTable ftable0 = new FingerTable(node0.getId());
+        Node node1 = new Node(1);
+        FingerTable ftable1 = new FingerTable(node1.getId());
+        Node node3 = new Node(3);
+        FingerTable ftable3 = new FingerTable(node3.getId());
+
+        node0.successor = node0;
+        node1.successor = node1;
+        node3.successor = node3;
+
+        node0.predecessor = node3;
+        node1.predecessor = node0;
+        node3.predecessor = node1;
+
+        ftable0.put(1, node1);
+        ftable0.put(2, node3);
+        ftable0.put(3, node0);
+
+        ftable1.put(1, node3);
+        ftable1.put(2, node3);
+        ftable1.put(3, node0);
+
+        ftable3.put(1, node0);
+        ftable3.put(2, node0);
+        ftable3.put(3, node0);
+
+        ftable0.prettyPrint();
+        ftable1.prettyPrint();
+        ftable3.prettyPrint();
+
+        node0.setFingerTable(ftable0);
+        node1.setFingerTable(ftable1);
+        node3.setFingerTable(ftable3);
+
+        System.out.println(node0.findSuccessor(node0).getId());
+
     }
 }
--- src/FingerTable.java
+++ src/FingerTable.java
@@ -2,58 +2,58 @@ import java.util.HashMap;
 
 class FingerTable {
     private int hostNodeId;
+    public static final int MAX_ENTRIES = 3;
     private HashMap<Integer, Node> fingerTable = new HashMap<>();
 
+    FingerTable(Integer hostNodeId) {
+        this.hostNodeId = hostNodeId;
+    }
 
+    public Node get(Integer entryNumber) {
+        if (entryNumber == 0) throw new RuntimeException("Entries start from 1");
+        if (entryNumber > MAX_ENTRIES) throw new RuntimeException("Exceeded number of entries. " +
+                "Only " + MAX_ENTRIES + " entries are allowed.");
+        Integer key = hash(entryNumber);
+        return this.fingerTable.get(key);
+    }
 
+    public void put(Integer entryNumber, Node node) {
+        if (entryNumber == 0) throw new RuntimeException("Entries start from 1");
+        if (entryNumber > MAX_ENTRIES) throw new RuntimeException("Exceeded number of entries. " +
+                "Only " + MAX_ENTRIES + " entries are allowed.");
+        Integer key = hash(entryNumber);
+        this.fingerTable.put(key, node);
+    }
 
+    public void prettyPrint() {
+        System.out.println("----------------------");
+        System.out.println("Finger Table - Node " + this.hostNodeId);
+        System.out.println("----------------------");
+        this.fingerTable.forEach((key, value) -> {
+            System.out.print(" Key: " + key);
+            System.out.println("      Id: " + value.getId());
+        });
+        System.out.println("----------------------");
+    }
 
+    private Integer hash(Integer entryNumber) {
+        //return this.hostNodeId + ((Math.pow(2,entryNumber)) % (Math.pow(2, MAX_ENTRIES)));
+        return this.hostNodeId + (int) ((Math.pow(2, entryNumber - 1)) % (Math.pow(2, MAX_ENTRIES)));
+    }
 
+    public static void main(String[] args) {
+        FingerTable f = new FingerTable(0);
 
+        //System.out.println(f.hash(3));
+        int ent1 = 3;
+        int ent2 = 2;
+        System.out.println(ent1 % Math.pow(2, MAX_ENTRIES));
+        System.out.println(ent2 % Math.pow(2, MAX_ENTRIES));
 
+        f.put(ent1, new Node(10));
+        f.put(ent2, new Node(1));
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-//    FingerTable(Node node) {
-//        this.hostNodeId = node.getId();
-//    }
-//
-//    FingerTable(int hostNodeId) {
-//        this.hostNodeId = hostNodeId;
-//    }
-//
-//    public int getHostNodeId() {
-//        return hostNodeId;
-//    }
-//
-//    public ChordNode get(int index) {
-//        // Essentially returns successor
-//        return this.fingerTable.get(index);
-//    }
-//
-//    public void set(int index, ChordNode successor) {
-//        fingerTable.put(index, successor);
-//    }
-//
-//    public HashMap<Integer, Node> getActualTable() {
-//        return this.fingerTable;
-//    }
-//
-//    protected void prettyPrint() {
-//
-//    }
-
-
+        System.out.println(f.get(ent1).getId());
+        System.out.println(f.get(ent2).getId());
+    }
 }
