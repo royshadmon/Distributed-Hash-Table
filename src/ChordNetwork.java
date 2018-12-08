931282d231106e32e63f364f0815a8dca6a1335b
Roy Shadmon <royshadmon@Roys-MacBook-Pro-3.local>
2018-12-07 17:37:38 -0800

    Printing keys for each node and updated Makefile as main function is now in ChordNetwork.java

--- src/ChordNetwork.java
+++ src/ChordNetwork.java
@@ -1,90 +1,138 @@
 import java.util.ArrayList;
+import java.util.List;
 
 public class ChordNetwork {
 
     public static void main(String[] args) {
+
+        // Make 2 lists of numbers from 0 to 255
+
+        List<Integer> NodeList = new ArrayList<>();
+        List <Integer> KeyList = new ArrayList<>();
+
+        for (int i = 0; i < FingerTable.MAX_NODES; i++) {
+            NodeList.add(i);
+            KeyList.add(i);
+        }
+
+        int totalNodes = 11;
+        int totalKeys = 13;
+
+        List<Node> nodes = new ArrayList<>();
+        List<Integer> keys = new ArrayList<>();
+
+        for (int i = 0; i < totalNodes; i++) {
+            int index = (int) (Math.random()*(255-i));
+            int num = NodeList.get(index);
+            System.out.println("Adding Node " + num);
+            nodes.add(new Node(num));
+            NodeList.remove(index);
+        }
+
+        for (int i = 0; i < totalKeys; i++) {
+            int index = (int) (Math.random() * (255-i));
+            int num = KeyList.get(index);
+
+            keys.add(num);
+            KeyList.remove(index);
+        }
+
+        Node node0 = nodes.get(0);
+        node0.join(null);
+
+        Node node1= nodes.get(1);
+        node1.join(node0);
+
+        node0.insert(keys.get(0));
+        node1.insert(keys.get(1));
+
+
+        Node node2 = nodes.get(2);
+        node2.join(node0);
+
+        Node node3 = nodes.get(3);
+        node3.join(node2);
+
+        Node node4 = nodes.get(4);
+        node4.join(node1);
+
+        node3.insert(keys.get(2));
+        node0.insert(keys.get(3));
+
+        Node node5 = nodes.get(5);
+        node5.join(node2);
+
+        node4.insert(keys.get(4));
+        node3.insert(keys.get(5));
+        node0.insert(keys.get(6));
+        node0.insert(keys.get(7));
+        node1.insert(keys.get(8));
+        node2.insert(keys.get(9));
+
+        Node node6 = nodes.get(6);
+        node6.join(node5);
+
+        Node node7 = nodes.get(7);
+        node7.join(node4);
+
+        Node node8 = nodes.get(8);
+        node8.join(node7);
+
+        node8.insert(keys.get(10));
+        node4.insert(keys.get(11));
+        node6.insert(keys.get(12));
+
+        Node node9 = nodes.get(9);
+        node9.join(node0);
+
+        Node node10 = nodes.get(10);
+        node10.join(node7);
+
+        System.out.println("------------Printing final node tables after inserting all nodes-----------");
+        node0.prettyPrint();
+        node1.prettyPrint();
+        node2.prettyPrint();
+        node3.prettyPrint();
+        node4.prettyPrint();
+        node5.prettyPrint();
+        node6.prettyPrint();
+        node7.prettyPrint();
+        node8.prettyPrint();
+        node9.prettyPrint();
+        node10.prettyPrint();
+
+
+        System.out.println("------------Nodes leave now, then reprint---------------");
+
+        node9.leave();
+        node0.leave();
+        node3.leave();
+        node6.leave();
+
+        node0.prettyPrint();
+        node1.prettyPrint();
+        node2.prettyPrint();
+        node3.prettyPrint();
+        node4.prettyPrint();
+        node5.prettyPrint();
+        node6.prettyPrint();
+        node7.prettyPrint();
+        node8.prettyPrint();
+        node9.prettyPrint();
+        node10.prettyPrint();
+
+
+
+//        System.out.println(node0.getKeys().get(0)==255);
+//        System.out.println(node10.getKeys().get(0)==0);
 //
-//        // Testing findSuccessor, findPredecessor and closestPrecedingFinger
-//        OldNode oldNode0 = new OldNode(0);
-//        OldFingerTable ftable0 = new OldFingerTable(oldNode0.getId());
-//        OldNode oldNode1 = new OldNode(1);
-//        OldFingerTable ftable1 = new OldFingerTable(oldNode1.getId());
-//        OldNode oldNode3 = new OldNode(3);
-//        OldFingerTable ftable3 = new OldFingerTable(oldNode3.getId());
+//        System.out.println(node1.getKeys().get(0)==2);
+//        System.out.println(node2.getKeys().get(0)==4);
+//        System.out.println(node3.getKeys().get(0)==8);
 //
-//        ftable0.put(1, oldNode1);
-//        ftable0.put(2, oldNode3);
-//        ftable0.put(3, oldNode0);
+//        System.out.println(node4.getKeys().size());
 //
-//        ftable1.put(1, oldNode3);
-//        ftable1.put(2, oldNode3);
-//        ftable1.put(3, oldNode0);
-//
-//        ftable3.put(1, oldNode0);
-//        ftable3.put(2, oldNode0);
-//        ftable3.put(3, oldNode0);
-//
-//        ftable0.prettyPrint();
-//        ftable1.prettyPrint();
-//        ftable3.prettyPrint();
-//
-//        oldNode0.setFingerTable(ftable0);
-//        oldNode1.setFingerTable(ftable1);
-//        oldNode3.setFingerTable(ftable3);
-//
-//        OldNode oldNode6 = new OldNode(6);
-//        oldNode6.join(oldNode3);
-//
-//        oldNode0.getFingerTable().prettyPrint();
-//        oldNode1.getFingerTable().prettyPrint();
-//        oldNode3.getFingerTable().prettyPrint();
-
-        OldNode oldNode0 = new OldNode(0);
-        OldNode oldNode1 = new OldNode(1);
-        OldNode oldNode3 = new OldNode(3);
-
-        ArrayList<Integer> keys0 = new ArrayList<>();
-        keys0.add(7);
-        keys0.add(3);
-        keys0.add(2);
-        keys0.add(1);
-        keys0.add(4);
-        keys0.add(0);
-        keys0.add(6);
-        keys0.add(5);
-
-        oldNode0.setKeys(keys0);
-
-        oldNode0.join(null);
-        oldNode1.join(oldNode0);
-        oldNode3.join(oldNode1);
-
-
-//        oldNode0.getFingerTable().prettyPrint();
-//        oldNode1.getFingerTable().prettyPrint();
-//        oldNode3.getFingerTable().prettyPrint();
-
-//        System.out.println("Now node 6 joins. Let's see the finger tables again.");
-
-        OldNode oldNode6 = new OldNode(6);
-        oldNode6.join(oldNode3);
-
-//        oldNode0.getFingerTable().prettyPrint();
-//        oldNode1.getFingerTable().prettyPrint();
-//        oldNode3.getFingerTable().prettyPrint();
-//        oldNode6.getFingerTable().prettyPrint();
-
-        System.out.println("Printing new keys in OldNode 6");
-        oldNode6.getKeys().forEach(key -> System.out.println(key));
-
-        System.out.println("Printing new keys in OldNode 0");
-        oldNode0.getKeys().forEach(key -> System.out.println(key));
-
-        System.out.println("Printing new keys in OldNode 1");
-        oldNode1.getKeys().forEach(key -> System.out.println(key));
-
-        System.out.println("Printing new keys in OldNode 3");
-        oldNode3.getKeys().forEach(key -> System.out.println(key));
+//        System.out.println(node5.getKeys().get(0)==17);
 
     }
 }
--- src/Node.java
+++ src/Node.java
@@ -137,7 +137,7 @@ public class Node {
      NODE LEAVE - Methods used exclusively for the node leave operation
      ***********************************************************************************************/
 
-    private void leave() {
+    public void leave() {
         // 0. Don't do anything if you are the last node
 
         // 1. Transfer all your keys to your successor
@@ -368,7 +368,16 @@ public class Node {
         return result;
     }
 
-    public void prettyPrint() { this.table.prettyPrint(); }
+    public void prettyPrint() {
+        this.table.prettyPrint();
+
+        // print keys
+        System.out.println("This node's keys are: ");
+        this.getKeys().forEach((key) -> {
+            System.out.print(key + ", ");
+        });
+        System.out.println("\n----------------------");
+    }
 
     public String toString() { return "" + this.getId(); }
 
@@ -381,138 +390,138 @@ public class Node {
      MAIN METHOD
      ************************************************************************************************/
 
-    public static void main(String[] args) {
-
-        // Make 2 lists of numbers from 0 to 255
-
-        List <Integer> NodeList = new ArrayList<>();
-        List <Integer> KeyList = new ArrayList<>();
-
-        for (int i = 0; i < FingerTable.MAX_NODES; i++) {
-            NodeList.add(i);
-            KeyList.add(i);
-        }
-
-        int totalNodes = 11;
-        int totalKeys = 13;
-
-        List<Node> nodes = new ArrayList<>();
-        List<Integer> keys = new ArrayList<>();
-
-        for (int i = 0; i < totalNodes; i++) {
-            int index = (int) (Math.random()*(255-i));
-            int num = NodeList.get(index);
-            System.out.println("Adding Node " + num);
-            nodes.add(new Node(num));
-            NodeList.remove(index);
-        }
-
-        for (int i = 0; i < totalKeys; i++) {
-            int index = (int) (Math.random() * (255-i));
-            int num = KeyList.get(index);
-
-            keys.add(num);
-            KeyList.remove(index);
-        }
-
-        Node node0 = nodes.get(0);
-        node0.join(null);
-
-        Node node1= nodes.get(1);
-        node1.join(node0);
-
-        node0.insert(keys.get(0));
-        node1.insert(keys.get(1));
-
-
-        Node node2 = nodes.get(2);
-        node2.join(node0);
-
-        Node node3 = nodes.get(3);
-        node3.join(node2);
-
-        Node node4 = nodes.get(4);
-        node4.join(node1);
-
-        node3.insert(keys.get(2));
-        node0.insert(keys.get(3));
-
-        Node node5 = nodes.get(5);
-        node5.join(node2);
-
-        node4.insert(keys.get(4));
-        node3.insert(keys.get(5));
-        node0.insert(keys.get(6));
-        node0.insert(keys.get(7));
-        node1.insert(keys.get(8));
-        node2.insert(keys.get(9));
-
-        Node node6 = nodes.get(6);
-        node6.join(node5);
-
-        Node node7 = nodes.get(7);
-        node7.join(node4);
-
-        Node node8 = nodes.get(8);
-        node8.join(node7);
-
-        node8.insert(keys.get(10));
-        node4.insert(keys.get(11));
-        node6.insert(keys.get(12));
-
-        Node node9 = nodes.get(9);
-        node9.join(node0);
-
-        Node node10 = nodes.get(10);
-        node10.join(node7);
-
-        System.out.println("------------Printing final node tables after inserting all nodes-----------");
-        node0.prettyPrint();
-        node1.prettyPrint();
-        node2.prettyPrint();
-        node3.prettyPrint();
-        node4.prettyPrint();
-        node5.prettyPrint();
-        node6.prettyPrint();
-        node7.prettyPrint();
-        node8.prettyPrint();
-        node9.prettyPrint();
-        node10.prettyPrint();
-
-
-        System.out.println("------------Nodes leave now, then reprint---------------");
-
-        node9.leave();
-        node0.leave();
-        node3.leave();
-        node6.leave();
-
-        node0.prettyPrint();
-        node1.prettyPrint();
-        node2.prettyPrint();
-        node3.prettyPrint();
-        node4.prettyPrint();
-        node5.prettyPrint();
-        node6.prettyPrint();
-        node7.prettyPrint();
-        node8.prettyPrint();
-        node9.prettyPrint();
-        node10.prettyPrint();
-
-
-
-//        System.out.println(node0.getKeys().get(0)==255);
-//        System.out.println(node10.getKeys().get(0)==0);
+//    public static void main(String[] args) {
 //
-//        System.out.println(node1.getKeys().get(0)==2);
-//        System.out.println(node2.getKeys().get(0)==4);
-//        System.out.println(node3.getKeys().get(0)==8);
+//        // Make 2 lists of numbers from 0 to 255
 //
-//        System.out.println(node4.getKeys().size());
+//        List <Integer> NodeList = new ArrayList<>();
+//        List <Integer> KeyList = new ArrayList<>();
 //
-//        System.out.println(node5.getKeys().get(0)==17);
-
-    }
+//        for (int i = 0; i < FingerTable.MAX_NODES; i++) {
+//            NodeList.add(i);
+//            KeyList.add(i);
+//        }
+//
+//        int totalNodes = 11;
+//        int totalKeys = 13;
+//
+//        List<Node> nodes = new ArrayList<>();
+//        List<Integer> keys = new ArrayList<>();
+//
+//        for (int i = 0; i < totalNodes; i++) {
+//            int index = (int) (Math.random()*(255-i));
+//            int num = NodeList.get(index);
+//            System.out.println("Adding Node " + num);
+//            nodes.add(new Node(num));
+//            NodeList.remove(index);
+//        }
+//
+//        for (int i = 0; i < totalKeys; i++) {
+//            int index = (int) (Math.random() * (255-i));
+//            int num = KeyList.get(index);
+//
+//            keys.add(num);
+//            KeyList.remove(index);
+//        }
+//
+//        Node node0 = nodes.get(0);
+//        node0.join(null);
+//
+//        Node node1= nodes.get(1);
+//        node1.join(node0);
+//
+//        node0.insert(keys.get(0));
+//        node1.insert(keys.get(1));
+//
+//
+//        Node node2 = nodes.get(2);
+//        node2.join(node0);
+//
+//        Node node3 = nodes.get(3);
+//        node3.join(node2);
+//
+//        Node node4 = nodes.get(4);
+//        node4.join(node1);
+//
+//        node3.insert(keys.get(2));
+//        node0.insert(keys.get(3));
+//
+//        Node node5 = nodes.get(5);
+//        node5.join(node2);
+//
+//        node4.insert(keys.get(4));
+//        node3.insert(keys.get(5));
+//        node0.insert(keys.get(6));
+//        node0.insert(keys.get(7));
+//        node1.insert(keys.get(8));
+//        node2.insert(keys.get(9));
+//
+//        Node node6 = nodes.get(6);
+//        node6.join(node5);
+//
+//        Node node7 = nodes.get(7);
+//        node7.join(node4);
+//
+//        Node node8 = nodes.get(8);
+//        node8.join(node7);
+//
+//        node8.insert(keys.get(10));
+//        node4.insert(keys.get(11));
+//        node6.insert(keys.get(12));
+//
+//        Node node9 = nodes.get(9);
+//        node9.join(node0);
+//
+//        Node node10 = nodes.get(10);
+//        node10.join(node7);
+//
+//        System.out.println("------------Printing final node tables after inserting all nodes-----------");
+//        node0.prettyPrint();
+//        node1.prettyPrint();
+//        node2.prettyPrint();
+//        node3.prettyPrint();
+//        node4.prettyPrint();
+//        node5.prettyPrint();
+//        node6.prettyPrint();
+//        node7.prettyPrint();
+//        node8.prettyPrint();
+//        node9.prettyPrint();
+//        node10.prettyPrint();
+//
+//
+//        System.out.println("------------Nodes leave now, then reprint---------------");
+//
+//        node9.leave();
+//        node0.leave();
+//        node3.leave();
+//        node6.leave();
+//
+//        node0.prettyPrint();
+//        node1.prettyPrint();
+//        node2.prettyPrint();
+//        node3.prettyPrint();
+//        node4.prettyPrint();
+//        node5.prettyPrint();
+//        node6.prettyPrint();
+//        node7.prettyPrint();
+//        node8.prettyPrint();
+//        node9.prettyPrint();
+//        node10.prettyPrint();
+//
+//
+//
+////        System.out.println(node0.getKeys().get(0)==255);
+////        System.out.println(node10.getKeys().get(0)==0);
+////
+////        System.out.println(node1.getKeys().get(0)==2);
+////        System.out.println(node2.getKeys().get(0)==4);
+////        System.out.println(node3.getKeys().get(0)==8);
+////
+////        System.out.println(node4.getKeys().size());
+////
+////        System.out.println(node5.getKeys().get(0)==17);
+//
+//    }
 
 
     /************************************************************************************************
